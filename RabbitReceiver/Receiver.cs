2f5ab23d4ae92a6bb2f0661729c16bb06bae450f
JJ Banda <james.f.banda@leidos.com>
2020-02-06 11:21:00 -0500

    added files

---
+++ RabbitReceiver/Receiver.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitReceiver
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                DispatchConsumersAsync = true,
+            };
+
+            Console.WriteLine("Making connections and channel...");
+            using var connection = factory.CreateConnection("Receiver");
+            using var taskChannel = connection.CreateModel();
+
+            taskChannel.QueueDeclare(queue: "tasks",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var taskConsumer = new AsyncEventingBasicConsumer(taskChannel);
+            taskConsumer.Received += async (sender, args) =>
+            {
+                Console.WriteLine("Got a message!");
+
+                try
+                {
+                    var cancel = connection.CreateModel();
+                    Console.WriteLine("Created a channel!");
+                    cancel.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                
+                await Task.CompletedTask;
+            };
+
+            taskChannel.BasicConsume(queue: "tasks",
+                                     autoAck: true,
+                                     consumer: taskConsumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitSender/Sender.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitTesting
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                DispatchConsumersAsync = false
+            };
+
+            using var connection = factory.CreateConnection("Sender");
+            using var taskChannel = connection.CreateModel();
+
+            taskChannel.QueueDeclare(queue: "tasks",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            bool shouldCancel = false;
+            while (!shouldCancel)
+            {
+                Console.WriteLine("Publishing message...");
+                taskChannel.BasicPublish(exchange: String.Empty,
+                                         routingKey: "tasks",
+                                         mandatory: false,
+                                         basicProperties: null,
+                                         body: Encoding.UTF8.GetBytes("hello"));
+
+                Console.WriteLine("Type something in to cancel; press [enter] to continue.");
+                var input = Console.ReadLine();
+
+                shouldCancel = !String.IsNullOrWhiteSpace(input);
+            }
+
+            Console.WriteLine(" Done ");
+        }
+    }
+}
